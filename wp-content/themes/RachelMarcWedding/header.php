2e82cf92dcc44172f9742e56d6734d927ab82753
Marisa Wilson <marisajune@gmail.com>
2016-07-13 14:10:22 -0700

    updates

--- wp-content/themes/RachelMarcWedding/header.php
+++ wp-content/themes/RachelMarcWedding/header.php
@@ -24,29 +24,6 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="content">
-			<div id="rach-marc">
-				<div><span>R</span>achel</div>
-				<div class="and"><span>&amp;</span></div>
-				<div><span>M</span>arc</div>
-			</div>
-			<img id="getting-married" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/getting-married2.png"/>
-			<div id="date-venue">
-				<div class="date">OCTOBER 23, 2016<span>four o'clock in the afternoon</span></div>
-				<div class="leaf">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/leaf.svg"/>
-				</div>
-				<div class="venue">DEER PARK VILLA<span>fairfax, california</span></div>
-			</div>
-			<div id="dinner-dancing">
-				<div class="dinner-dancing">dinner <span class="and"><span>&amp;</span></span> dancing to follow</div>
-				<div class="leaf">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/leaf.svg"/>
-				</div>
-				<div class="adults">adults only please</div>
-			</div>
-			<div id="rsvp">
-				<a href="/rsvp">RSVP Here</a>
-			</div>
 		</div>
 	</header>
 
--- wp-content/themes/RachelMarcWedding/page-templates/home.php
+++ wp-content/themes/RachelMarcWedding/page-templates/home.php
@@ -5,7 +5,32 @@ Template Name: Home
 get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-<section class="intro" role="main">
+<section id="front" class="intro" role="main">
+		<div class="content">
+			<div id="rach-marc">
+				<div><span>R</span>achel</div>
+				<div class="and"><span>&amp;</span></div>
+				<div><span>M</span>arc</div>
+			</div>
+			<img id="getting-married" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/getting-married2.png"/>
+			<div id="date-venue">
+				<div class="date">OCTOBER 23, 2016<span>four o'clock in the afternoon</span></div>
+				<div class="leaf">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/leaf.svg"/>
+				</div>
+				<div class="venue">DEER PARK VILLA<span>fairfax, california</span></div>
+			</div>
+			<div id="dinner-dancing">
+				<div class="dinner-dancing">dinner <span class="and"><span>&amp;</span></span> dancing to follow</div>
+				<div class="leaf">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/leaf.svg"/>
+				</div>
+				<div class="adults">adults only please</div>
+			</div>
+			<div id="rsvp">
+				<a href="/rsvp">RSVP Here</a>
+			</div>
+		</div>
 
 </section>
 <?php endwhile;?>
